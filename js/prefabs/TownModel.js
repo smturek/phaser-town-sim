2bb0d6c9eb761e144daec7932f00764df3cd8735
smturek <smturek@gmail.com>
2015-09-09 14:58:34 -0600

    added population growth

--- js/prefabs/TownModel.js
+++ js/prefabs/TownModel.js
@@ -1,5 +1,17 @@
 var HTown = HTown || {};
 
-HTown.TownModel = function() {
+HTown.TownModel = function(coefs, initialStats) {
 
+    this.coefs = {};
+    this.coefs.populationGrowth = coefs.populationGrowth || 1.02;
+
+    this.stats = {};
+    this.stats.population = initialStats.population;
+    this.stats.food = initialStats.food;
+    this.stats.money = initialStats.money;
+
+};
+
+HTown.TownModel.prototype.step = function() {
+    this.stats.population = this.stats.population * this.coefs.populationGrowth;
 };
