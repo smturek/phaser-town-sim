ad53199b112fcc5671fdc2cffdf1fe8c2c22031d
smturek <smturek@gmail.com>
2015-09-09 22:16:04 -0600

    added population restrictions

--- js/prefabs/TownModel.js
+++ js/prefabs/TownModel.js
@@ -1,6 +1,6 @@
 var HTown = HTown || {};
 
-HTown.TownModel = function(coefs, initialStats) {
+HTown.TownModel = function(coefs, initialStats, buildings) {
 
     this.coefs = {};
     this.coefs.populationGrowth = coefs.populationGrowth || 1.02;
@@ -10,8 +10,25 @@ HTown.TownModel = function(coefs, initialStats) {
     this.stats.food = initialStats.food;
     this.stats.money = initialStats.money;
 
+    this.buildings = buildings;
+
 };
 
 HTown.TownModel.prototype.step = function() {
+    this.updateBuildingProduction();
+
     this.stats.population = this.stats.population * this.coefs.populationGrowth;
+
+    //population cannot be greater that housing allows
+    this.stats.population = Math.min(this.stats.population, this.stats.housing)
+};
+
+HTown.TownModel.prototype.updateBuildingProduction = function() {
+    this.stats.housing = 0;
+
+    this.buildings.forEach(function(building){
+        if(building.housing) {
+            this.stats.housing += building.housing;
+        }
+    }, this);
 };
